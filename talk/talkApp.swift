e6b5ff9ced1bd5eb910a20fccde558605b5e48b1
meagancojocar <50200557+meagancojocar@users.noreply.github.com>
2021-11-30 21:53:38 -0800

    Add files via upload

---
+++ talk/talkApp.swift
@@ -0,0 +1,17 @@
+//
+//  talkApp.swift
+//  talk
+//
+//  Created by Meagan Cojocar on 11/30/21.
+//
+
+import SwiftUI
+
+@main
+struct talkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
